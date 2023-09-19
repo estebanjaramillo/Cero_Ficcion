8f07f408619f64e1be47de38756764c8e68e2c71
esteban jaramillo gonzalez <gaymerotaku2016@gmail.com>
2023-09-18 19:00:30 -0500

    Primer commit

---
+++ movies/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    director = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title +' - '+ self.director
+
+    class Meta:
+        permissions = [
+            ("can_add_movie", "Can add movie"),
+        ]
