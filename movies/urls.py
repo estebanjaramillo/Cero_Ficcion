31b0b97f2a798b35d024886df6632b110af46ab0
esteban jaramillo gonzalez <gaymerotaku2016@gmail.com>
2023-09-21 17:44:20 -0500

    Cambio de url para la pagina principal

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movies/', views.movie_list, name='movie_list'),
+    path('', views.movie_list, name='movie_list'),
     path('login/',views.login, name='login')
 ]
