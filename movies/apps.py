07ada2487286c38684abb1314f55c73ab1718b8a
estebanjaramill0 <esteban_jaramillo23221@elpoli.edu.co>
2023-10-10 10:41:34 -0500

    creacion de los modulos de calificacion y asistencia

--- movies/apps.py
+++ movies/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'  # Asegúrate de reemplazar 'movies' con el nombre de tu aplicación
+
